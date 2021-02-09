0d4ba39644232c24f756e6c9c2d4a143785de68b
krasosu <krasosu@krasosu.de>
2021-02-09 09:29:14 +0100

    configure swagger

--- src/main/java/de/krasosu/spring/data/es/SpringDataElasticsearchApplication.java
+++ src/main/java/de/krasosu/spring/data/es/SpringDataElasticsearchApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDataElasticsearchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataElasticsearchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataElasticsearchApplication.class, args);
+    }
 
 }
--- src/main/java/de/krasosu/spring/data/es/config/SwaggerConfig.java
+++ src/main/java/de/krasosu/spring/data/es/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
-public class SpringFoxConfig {
+public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
