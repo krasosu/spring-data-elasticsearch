80952f6e8ef5603bf754b498ad2c3a4a51fffa79
krasosu <krasosu@krasosu.de>
2021-02-09 08:46:07 +0100

    spring initializr

---
+++ src/test/java/de/krasosu/spring/data/es/SpringDataElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.krasosu.spring.data.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
